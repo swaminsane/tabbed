118b0593b2316693952ca022602a86e53b4ca5f0
Derek Taylor <contact@unabot.com>
2020-03-27 19:04:35 -0500

    Changing keybindings to better work with autohide.

--- config.h
+++ config.h
@@ -40,13 +40,13 @@ static Bool npisrelative  = False;
 #define MODKEY ControlMask
 static Key keys[] = {
 	/* modifier             key        function     argument */
-	{ MODKEY|Mod1Mask,     XK_Return, focusonce,   { 0 } },
-	{ MODKEY|Mod1Mask,     XK_Return, spawn,       { 0 } },
+	{ MODKEY|Mod1Mask,      XK_Return, focusonce,   { 0 } },
+	{ MODKEY|Mod1Mask,      XK_Return, spawn,       { 0 } },
 
-	{ MODKEY|Mod1Mask,     XK_l,      rotate,      { .i = +1 } },
-	{ MODKEY|Mod1Mask,     XK_h,      rotate,      { .i = -1 } },
-	{ MODKEY|Mod1Mask,     XK_j,      movetab,     { .i = -1 } },
-	{ MODKEY|Mod1Mask,     XK_k,      movetab,     { .i = +1 } },
+	{ MODKEY|Mod1Mask,      XK_l,      rotate,      { .i = +1 } },
+	{ MODKEY|Mod1Mask,      XK_h,      rotate,      { .i = -1 } },
+	{ MODKEY|Mod1Mask,      XK_j,      movetab,     { .i = -1 } },
+	{ MODKEY|Mod1Mask,      XK_k,      movetab,     { .i = +1 } },
 	{ MODKEY,               XK_Tab,    rotate,      { .i = 0 } },
 
 	{ MODKEY,               XK_grave,  spawn,       SETPROP("_TABBED_SELECT_TAB") },
@@ -61,7 +61,7 @@ static Key keys[] = {
 	{ MODKEY,               XK_9,      move,        { .i = 8 } },
 	{ MODKEY,               XK_0,      move,        { .i = 9 } },
 
-	{ MODKEY,               XK_q,      killclient,  { 0 } },
+	{ MODKEY|Mod1Mask,      XK_q,      killclient,  { 0 } },
 
 	{ MODKEY,               XK_u,      focusurgent, { 0 } },
 	{ MODKEY|Mod1Mask,      XK_u,      toggle,      { .v = (void*) &urgentswitch } },
